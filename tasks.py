483b14a12b54113dbacff35646ec209d7729970c
Jan Vansteenkiste <jan@vstone.eu>
2017-07-07 08:45:32 +0200

    Add a task to lint (using flake8)

--- tasks.py
+++ tasks.py
@@ -1,2 +1,19 @@
 """Invoke tasks."""
+import glob
 from invoke import task
+
+
+@task(
+    aliases=["flake8", "pep8"],
+    help={
+        'filename': 'File(s) to lint. Supports globbing.',
+    },
+)
+def lint(ctx, filename=None):
+    """Run flake8 python linter."""
+    command = 'flake8'
+    if filename is not None:
+        files = [x for x in glob.glob(filename)]
+        command += ' ' + " ".join(files)
+
+    ctx.run(command)
